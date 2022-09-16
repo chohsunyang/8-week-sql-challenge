68a1e188822190996c8a59f18af4296e33dccd56
Michael C. Yu <40799314+chohsunyang@users.noreply.github.com>
2022-09-16 16:03:03 +0800

    Update solutionA.sql

--- Case Study #2/solutionA.sql
+++ Case Study #2/solutionA.sql
@@ -2,3 +2,19 @@
 -- Case Study Questions --
 --------------------------
 
+-- 1. How many pizzas were ordered?
+SELECT COUNT(r.pizza_id) AS pizza_orders
+FROM pizza_runner.customer_orders r
+
+-- 2. How many unique customer orders were made?
+SELECT COUNT(DISTINCT r.order_id) AS orders
+FROM pizza_runner.customer_orders r
+
+-- 3. How many successful orders were delivered by each runner?
+-- 4. How many of each type of pizza was delivered?
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+-- 6. What was the maximum number of pizzas delivered in a single order?
+-- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
+-- 10. What was the volume of orders for each day of the week?
