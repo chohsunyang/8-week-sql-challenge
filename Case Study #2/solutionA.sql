b6f6ce5766a36bf18846834bc53f688a65ed85bc
Michael C. Yu <40799314+chohsunyang@users.noreply.github.com>
2022-09-23 13:59:40 +0800

    Update solutionA.sql

--- Case Study #2/solutionA.sql
+++ Case Study #2/solutionA.sql
@@ -38,7 +38,48 @@ LEFT JOIN #pizza_names p
 GROUP BY p.pizza_name, C.customer_id
 
 -- 6. What was the maximum number of pizzas delivered in a single order?
+SELECT TOP(1)
+       c.order_id,
+       COUNT(c.pizza_id) AS pizza_delivered
+FROM #runner_orders_clean r 
+LEFT JOIN #customer_orders_clean c
+       ON r.order_id = c.order_id
+WHERE r.duration IS NOT NULL
+GROUP BY c.order_id
+ORDER BY COUNT(c.pizza_id) DESC
+
 -- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+SELECT c.customer_id,
+       SUM(CASE WHEN c.extras IS NULL AND c.exclusions IS NULL THEN 1
+	            ELSE 0 END) AS pizza_no_change,
+       SUM(CASE WHEN c.extras IS NOT NULL OR c.exclusions IS NOT NULL THEN 1
+	            ELSE 0 END) AS pizza_with_change,
+       COUNT(c.pizza_id) AS pizza_delivered
+FROM #runner_orders_clean r 
+LEFT JOIN #customer_orders_clean c
+       ON r.order_id = c.order_id
+WHERE r.duration IS NOT NULL
+GROUP BY c.customer_id
+
 -- 8. How many pizzas were delivered that had both exclusions and extras?
+SELECT COUNT(c.pizza_id) AS pizza_delivered
+FROM #runner_orders_clean r 
+LEFT JOIN #customer_orders_clean c
+       ON r.order_id = c.order_id
+WHERE r.duration IS NOT NULL
+  AND c.extras IS NOT NULL 
+  AND c.exclusions IS NOT NULL
+
 -- 9. What was the total volume of pizzas ordered for each hour of the day?
+SELECT DATEPART(HOUR, order_time) AS hour_of_day,
+       COUNT(order_id) AS pizza_order
+FROM #customer_orders_clean
+GROUP BY DATEPART(HOUR, order_time)
+      
+
 -- 10. What was the volume of orders for each day of the week?
+SELECT FORMAT(order_time, 'dddd') AS day_of_week,
+       COUNT(order_id) AS pizza_order
+FROM #customer_orders_clean
+GROUP BY FORMAT(order_time, 'dddd')
+ORDER BY FORMAT(order_time, 'dddd')
